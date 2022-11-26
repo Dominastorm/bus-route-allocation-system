2dbeedf2f053653bc60ad4f6e14d4f648c58efea
Dhruv Chawla <dchawla228@gmail.com>
2022-11-26 20:53:23 +0530

    Add logo

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
+from PIL import Image
 
 def main() -> None:
     # Load the configuration file
@@ -17,22 +18,29 @@ def main() -> None:
         config['preauthorized']
     )
 
+    # Add a logo to the sidebar
+    image = Image.open('assets/images/logo.png')
+    st.sidebar.image(image, use_column_width=True)
+    st.sidebar.title('Bus Route Allocation System')
+    
+
     # Authenticate the user
     name, authentication_status, username = authenticator.login('Login', 'main')
 
     # If the user is authenticated, display the app
     if authentication_status:
         # SIDEBAR
-        menu = ['Home']
+        menu = ['Home', 'My Profile', ]
         choice = st.sidebar.selectbox('Navigation Menu', menu)
-
         
         st.sidebar._html('<br>')
+        # Logout
         authenticator.logout('Logout', 'sidebar')
-
-
+    
         # MAIN
         st.write(f'Welcome, *{name}*!')
+
+
         # Allow user to change password
         try:
             if authenticator.reset_password(username, 'Reset password', 'sidebar'):
@@ -40,9 +48,11 @@ def main() -> None:
                 with open('./config.yaml', 'w') as file:
                     yaml.dump(config, file, default_flow_style=False)
                 st.sidebar.success('Password changed successfully')
+
         except Exception as e:
             st.error(e)
 
+        
     # Incorrect username or password
     elif authentication_status == False:
         st.error('Username/password is incorrect')
