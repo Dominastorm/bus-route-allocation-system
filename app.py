3d0c7be82fc138685ca9d88c29f17e97f6fb8a7d
Dhruv Chawla <dchawla228@gmail.com>
2022-11-26 16:36:48 +0530

    Add login page

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
 import streamlit as st
 import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 
+
+# Load the configuration file
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+# Create the authenticator
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+# Authenticate the user
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+# If the user is authenticated, display the app
+if authentication_status:
+    authenticator.logout('Logout', 'sidebar')
+    st.write(f'Welcome, *{name}*!')
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+    
